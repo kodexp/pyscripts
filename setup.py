6c335560f10223f24af5aa272ad89ca858155601
feizhanga <fei2zhang@gmail.com>
2013-12-27 15:05:59 +1100

    tox auto tool

---
+++ setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python2.7
+
+from __future__ import print_function
+from distutils.core import setup
+import os
+
+version = '0.1'
+
+# Append TeamCity build number if it gives us one.
+if 'TC_BUILD_NUMBER' in os.environ and version.endswith('b'):
+    version += '-' + os.environ['TC_BUILD_NUMBER']
+
+setup(
+    name='hotpies',
+    url='http://github.con',
+    maintainer='Fei Zhang',
+    maintainer_email='fei.zhang@ga.gov.au',
+    version=version,
+    description='Python code samples ',
+    packages=[
+        'hellopack',
+    ],
+    scripts=[
+        'script/scripta',
+    ],
+    requires=[
+        'pika',
+        'requests',
+        'suds',
+        'mock'
+    ]
+)
